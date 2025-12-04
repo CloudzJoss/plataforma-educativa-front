d4fa184cce8fbb23674d6f949f37fc44c2955079
lcalderoni-arch <lcalderoni@autonoma.edu.pe>
2025-12-03 23:21:58 -0500

    ARREGLO, AÑADIDO DE DISEÑO v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,10 +48,10 @@ export default function Header() {
         {/* Menú deslizable */}
         <div className={`nav-links-wrapper ${menuOpen ? 'open' : ''}`}>
           <div className="nav-links">
-          <a className="other-page" href="#nosotros">Nosotros</a>
-          <a className="other-page" href="#programas">Programas</a>
-          <a className="other-page" href="#contactanos">Contáctanos</a>
-        </div>
+            <a className="other-page" href="#nosotros" onClick={handleLinkClick}>Nosotros</a>
+            <a className="other-page" href="#programas" onClick={handleLinkClick}>Programas</a>
+            <a className="other-page" href="#contactanos" onClick={handleLinkClick}>Contáctanos</a>
+          </div>
         </div>
 
         {/* Botón de sesión (siempre visible) */}
